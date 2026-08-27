759b6bdc1372271a181c0ba0fb0b44c77f4d4394
Adrian <adrian.undertale@gmail.com>
2026-08-27 16:36:15 +0000

    js-calculator class activity submitted

--- Classwork/js-calculator/js/calculator.js
+++ Classwork/js-calculator/js/calculator.js
@@ -12,16 +12,31 @@ const resultStatus = document.getElementById('result-status');
 // ==========================================
 
 // TODO: Write "add" callback expression (a, b) => ...
-
+function add(a , b){
+    return a + b;
+}
 
 // TODO: Write "subtract" callback expression (a, b) => ...
+function subtract(a,b){
+    return a - b;
+}
 
 
 // TODO: Write "multiply" callback expression (a, b) => ...
+function multiply(a,b){
+    return a * b;
+}
 
 
 // TODO: Write "divide" callback expression (a, b) => ...
 // Rule: Guard against division-by-zero! Return an Error or string warning.
+function divide(a,b){
+    try {
+        return a/b;
+    } catch (error) {
+        throw new Error ("Can not divide by zero");
+    }
+}
 
 
 // ==========================================
@@ -34,20 +49,54 @@ const resultStatus = document.getElementById('result-status');
 //   - Is numA and numB actually valid numbers?
 //   - Is callback actually a function?
 // Execution: Returns callback(numA, numB)
-
+function calculator (numA, numB, callback){
+    return callback(numA, numB);
+    
+}
 
 // ==========================================
 // 4. TODO: EVENT OBSERVER & INTEGRATION WIRING (Students write this)
 // ==========================================
 calculateBtn.addEventListener('click', () => {
-    alert('click');
+try {
+    // alert('click');
     // TODO: Extract values from the inputs and parse them as floats.
+    const valueA = parseFloat(num1Input.value);
+    const valueB = parseFloat(num2Input.value);
 
     // TODO: Retrieve the selected operation string value.
-
+    const operation = operationSelect.value;
+    if (num1Input.value === "" || num2Input.value === ""){
+        throw new Error("Introduce a new number");
+    }
+    if (operation.value === ""){
+        throw new Error ("Selection an operation");
+    }
     // TODO: Match the selected operation string to its corresponding function reference.
+    let targetCallback;
+    switch (operation){
+        case "add":
+            targetCallback = add;
+            break;
+        case "substract":
+            targetCallback = subtract;
+            break;
+        case "multiply":
+            targetCallback = multiply;
+            break;
+        case "divide":
+            targetCallback = divide;
+            break;
+    }
 
     // TODO: Execute the higher-order 'calculator' function with input values and the matched function reference.
+    const ans = calculator(valueA, valueB, targetCallback);
 
     // TODO: Update resultStatus text, toggling classes (e.g., alert-success vs alert-danger) based on outcomes!
+    resultStatus.className = "alert alert-success text center"
+    resultStatus.textContent = `Result: ${ans}`;
+    } catch (error) {
+        resultStatus.className = "alert alert-danger text center"
+        resultStatus.textContent = `Error: ${error.message}`
+    }
 });
